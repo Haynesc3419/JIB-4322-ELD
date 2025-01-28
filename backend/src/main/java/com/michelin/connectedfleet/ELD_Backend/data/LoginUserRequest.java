80fd892078283b8cd6675aaf2883a1d4b3976580
andrewyvng <143544070+andrewyvng@users.noreply.github.com>
2025-01-27 20:38:57 -0500

    Create LoginUserRequest.java

---
+++ backend/src/main/java/com/michelin/connectedfleet/ELD_Backend/data/LoginUserRequest.java
@@ -0,0 +1,7 @@
+package com.michelin.connectedfleet.ELD_Backend.data;
+
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.data.annotation.Id;
+
+public record LoginUserRequest(@NotBlank String username, @NotBlank String password) {
+}
