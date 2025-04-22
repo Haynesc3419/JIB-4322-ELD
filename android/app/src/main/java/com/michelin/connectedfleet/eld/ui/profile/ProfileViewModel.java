bbd9b06e5cc56baaf5c6a38fdad67d9086b5d716
andrewyvng <143544070+andrewyvng@users.noreply.github.com>
2025-04-21 20:33:12 -0400

    removed unused methods and classes

--- android/app/src/main/java/com/michelin/connectedfleet/eld/ui/profile/ProfileViewModel.java
+++ android/app/src/main/java/com/michelin/connectedfleet/eld/ui/profile/ProfileViewModel.java
@@ -7,20 +7,20 @@ import androidx.lifecycle.ViewModel;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ProfileViewModel extends ViewModel {
-
-    private final MutableLiveData<List<String>> mTexts;
-
-    public ProfileViewModel() {
-        mTexts = new MutableLiveData<>();
-        List<String> texts = new ArrayList<>();
-        for (int i = 1; i <= 16; i++) {
-            texts.add("This is item # " + i);
-        }
-        mTexts.setValue(texts);
-    }
-
-    public LiveData<List<String>> getTexts() {
-        return mTexts;
-    }
-}
+//public class ProfileViewModel extends ViewModel {
+//
+//    private final MutableLiveData<List<String>> mTexts;
+//
+//    public ProfileViewModel() {
+//        mTexts = new MutableLiveData<>();
+//        List<String> texts = new ArrayList<>();
+//        for (int i = 1; i <= 16; i++) {
+//            texts.add("This is item # " + i);
+//        }
+//        mTexts.setValue(texts);
+//    }
+//
+//    public LiveData<List<String>> getTexts() {
+//        return mTexts;
+//    }
+//}
